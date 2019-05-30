7a5a7f2a0b7022d7fafd0efddae244f24a08f54a
Leticia Pereira <lps.lets@gmail.com>
2019-05-30 11:00:10 -0300

    adicionar todos os arquivos

---
+++ scripts/para-baixar-estilos-de-citacao.R
@@ -0,0 +1,4 @@
+if(!require("devtools")) install.packages("devtools")
+if(!require("minimalTemplate")) devtools::install_github("kguidonimartins/minimalTemplate",force=TRUE)
+devtools::install_github("kguidonimartins/minimalTemplate",force=TRUE)
+
